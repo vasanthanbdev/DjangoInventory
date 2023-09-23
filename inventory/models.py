501a46ab3f308a3be4f946edb9b22c399d73bee4
vasanthanb <vasanthan_b@outlook.com>
2023-09-23 18:22:31 +0530

    product and item models

--- inventory/models.py
+++ inventory/models.py
@@ -1,5 +1,27 @@
 from django.db import models
+import uuid
 
 # Create your models here.
 class Product(models.Model):
-    pass
+    name = models.CharField(max_length=100)
+    description = models.TextField(blank=True, null=True)
+    quantity = models.PositiveIntegerField(null=True)
+    purchase_price = models.DecimalField(max_digits=10, decimal_places=2)
+    sale_price = models.DecimalField(max_digits=10, decimal_places=2)
+    
+    def __str__(self) -> str:
+        return self.name
+    
+class Item(models.Model):
+    serial_number = models.CharField(max_length=50, unique=True)
+    product = models.ForeignKey(Product, on_delete=models.CASCADE)
+
+    def save(self, *args, **kwargs):
+        if not self.serial_number:
+            # Generate a unique serial number (e.g., using UUID)
+            self.serial_number = str(uuid.uuid4())
+        super(Item, self).save(*args, **kwargs)
+    
+    def __str__(self) -> str:
+        return f"{self.product} - Serial Number: {self.serial_number}"
+
